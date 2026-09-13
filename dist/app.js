82f9059bffdc3dac680a740cee0d29496fd5cf7c
gironflorezfabionelson-ship-it <gironflorezfabionelson@gmail.com>
2026-09-14 00:44:39 +0200

    Load product image fixes after app core

--- dist/app.js
+++ dist/app.js
@@ -8,6 +8,7 @@
   });
 
   loadScript('app-core.js?v=5')
+    .then(() => loadScript('product-image-fixes.js?v=1'))
     .then(() => loadScript('traffic-start.js?v=2'))
     .catch((err) => console.error('CompraSmart script load error', err));
 })();
