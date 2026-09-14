4797be2bdbfed894bb6fcf45cc92f17693f3006d
gironflorezfabionelson-ship-it <gironflorezfabionelson@gmail.com>
2026-09-14 16:11:47 +0200

    Load conversion improvements

--- dist/app.js
+++ dist/app.js
@@ -10,5 +10,6 @@
   loadScript('app-core.js?v=5')
     .then(() => loadScript('product-image-fixes.js?v=1'))
     .then(() => loadScript('traffic-start.js?v=2'))
+    .then(() => loadScript('conversion-boost.js?v=1'))
     .catch((err) => console.error('CompraSmart script load error', err));
 })();
